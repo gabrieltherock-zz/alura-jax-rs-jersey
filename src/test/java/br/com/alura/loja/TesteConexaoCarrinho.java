39171a50b98709d5b99bc96e04f15b820379561b
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-07-17 14:39:24 -0300

    adicionando testes de conexao

---
+++ src/test/java/br/com/alura/loja/TesteConexaoCarrinho.java
@@ -0,0 +1,38 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.modelo.Carrinho;
+import com.thoughtworks.xstream.XStream;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+public class TesteConexaoCarrinho {
+
+    private HttpServer server;
+
+    @Before
+    public void before() {
+        this.server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
+
+    @Test
+    public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/carrinhos").request().get(String.class);
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+    }
+
+}
--- src/test/java/br/com/alura/loja/TesteConexaoProjeto.java
+++ src/test/java/br/com/alura/loja/TesteConexaoProjeto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
 import com.thoughtworks.xstream.XStream;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -12,7 +13,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-public class TesteConexao {
+public class TesteConexaoProjeto {
 
     private HttpServer server;
 
@@ -27,12 +28,11 @@ public class TesteConexao {
     }
 
     @Test
-    public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+    public void testaQueBuscarUmCarrinhoTrazOProjetoEsperado() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
-        String conteudo = target.path("/carrinhos").request().get(String.class);
-        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
-        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+        String conteudo = target.path("/projetos").request().get(String.class);
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
     }
-
 }
